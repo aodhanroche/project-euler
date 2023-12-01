852feda77326f19ad7b15f9b1dd309bd4bc97bdf
Aodhan Roche <aodhan@griptape.ai>
2023-12-01 15:27:34 -0800

    Added a check for valid target, and a type hint for the function

--- 24_lexicographic_permutations/24_lexicographic_permutations.py
+++ 24_lexicographic_permutations/24_lexicographic_permutations.py
@@ -1,23 +1,37 @@
 from itertools import islice, permutations
+import math
 
 
-def lexicographically_permutate(tuple_of_digits, target):
-    # set a variable for the itertools permutation of the tuple
-    digit_permutations = permutations(tuple_of_digits)
+def lexicographically_permutate(tuple_of_digits, target) -> str:
 
-    # slice the digit_permutation obj to the permutation of the tuple you are targeting
-    digits = next(
-        islice(
-            digit_permutations, target - 1, target
+    # calculate the total number of permutations that are possible
+    possible_permutations = math.factorial(len(tuple_of_digits))
+
+    # if the target is less than the total permutations:
+    if target <= possible_permutations:
+
+        # set a variable for the itertools permutation of the tuple
+        digit_permutations = permutations(tuple_of_digits)
+
+        # slice the digit_permutation obj to the permutation of the tuple you are targeting
+        digits = next(
+            islice(
+                digit_permutations, target - 1, target
+
+            )
         )
-    )
 
-    # join the tuple digits into a str
-    ans = ''.join(map(str, digits))
+        # join the tuple digits into a str
+        ans = ''.join(map(str, digits))
 
-    return ans
+        # return the permutation of interest
+        return ans
+
+    # if there are fewer permutations than the target:
+    else:
+
+        # return an error message
+        return f"There are not {target} permutations of those numbers"
 
 
 print(lexicographically_permutate((0, 1, 2, 3, 4, 5, 6, 7, 8, 9), 1000000))
-
-
